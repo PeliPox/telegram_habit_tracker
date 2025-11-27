72d3b1b4bbda97a3eae8bd93fe6185f395a0e8c7
dmitriypelipenko <dima_pelipenko13@mail.ru>
2025-11-27 13:57:46 +0300

    Добавлены кнопки для удаления привычек (предварительно). Создан файл menu (handler меню бота).

--- bot/handlers/habits.py
+++ bot/handlers/habits.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from db.base import get_db
 from db.crud import get_habits_by_user, get_user
 from aiogram.filters import Command
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 router = Router()
 
@@ -17,10 +18,15 @@ async def list_habits(message: types.Message):
     text = " \bТвои привычки: \n\n"
 
     if not habits:
-        await message.answer("У тебя пока нет привычек.")
+        await message.answer("У тебя пока нет привычек. Добавь через команду /add_habit")
     else:
+        keyboard = InlineKeyboardBuilder()
         for h in habits:
             text += f"•{h.title} - каждые {h.periodicity} дней\n"
-        await message.answer(text=text, parse_mode="Markdown")
+            keyboard.button(
+                text=f'Удалить {h.title}',
+                callback_data=f"delete_habit:{h.id}"
+            )
+        await message.answer(text=text, reply_markup=keyboard.as_markup(), parse_mode="Markdown")
 
     next(db_gen, None)
