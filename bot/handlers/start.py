80619c50075c8a6682833d0d7ec259902a2e77d0
dmitriypelipenko <dima_pelipenko13@mail.ru>
2025-12-09 19:14:34 +0300

    доработал команду /start добавив две кнопки (создания и список)

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -2,6 +2,7 @@ from aiogram import Router, types
 from aiogram.filters import Command
 from db.base import SessionLocal
 import db.crud as crud
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, KeyboardButton
 
 router = Router()
 
@@ -13,5 +14,22 @@ async def cmd_start(message: types.Message):
     if not user:
         user = crud.create_user(db, message.from_user.id, message.from_user.first_name)
 
-    await message.answer(f'Привет, {user.name}! Я - твой личный трекер привычек.')
+    welcome_text = (f"Привет, {user.name}! Я - твой личный трекер привычек.\n"
+                    f"Начнем?")
+
+    keyboard = ReplyKeyboardBuilder()
+    keyboard.add(
+        KeyboardButton(text="➕ Создать привычку"),
+        KeyboardButton(text="📋 Мои привычки")
+    )
+    keyboard.adjust(1)
+
+    await message.answer(
+        welcome_text,
+        reply_markup=keyboard.as_markup(
+            resize_keyboard=True,
+            one_time_keyboard=False
+        )
+    )
+    # await message.answer(f'Привет, {user.name}! Я - твой личный трекер привычек.')
 
